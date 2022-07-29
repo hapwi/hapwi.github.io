c145f9c7e55cec7add1424a33c500f52819277b6
Hap Wi <blangpab@gmail.com>
2022-07-29 14:55:03 -0700

    test

--- js/index.js
+++ js/index.js
@@ -31,8 +31,9 @@
 
 
 {
-  if (new Date() >= new Date("07/21/22 9:00")) {
-    location.href = 'http://www.golf.contact/beback.html';
-  } 
+  if (new Date() >= new Date("08/01/22 9:00")) {
+    location.href = 'http://golfpooll.herokuapp.com/beback.html';
+  }
+
 
 }
