110fc5b6401501542288b3a30fb28f8689427e56
Hap Wi <blangpab@gmail.com>
2022-07-29 15:11:43 -0700

    test

--- js/maint.js
+++ js/maint.js
@@ -7,9 +7,6 @@
 {
     if (new Date() >= new Date("08/01/22 9:00")) {
       location.href = 'http://www.golf.contact/beback.html';
-    } else{
-      location.href = 'http://www.golf.contact/';
-    }
-  
+    } 
   
   }
