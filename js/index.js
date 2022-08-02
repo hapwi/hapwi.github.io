d9393d5f31a6de15bf805b210fa94acd4060eae1
Hap Wi <blangpab@gmail.com>
2022-08-02 09:31:43 -0700

    test mobile

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@
 {
 
 
-  if (new Date() >= new Date("Thu Jul 28 2022 9:00")) {
+  if (new Date() >= new Date("07/11/22 9:00")) {
 
     document.getElementById("link92").innerHTML += " <a href='../subs.html' class='menu__link r-link'>Entries</a>";
 
   } else {
 
-    document.getElementById("link92").innerHTML += "Submissions (Locked)";
+    document.getElementById("link92").innerHTML += "Entries (Locked)";
   }
 }
 
 // lock form when tournament starts
 {
-  if (new Date() >= new Date("Aug 28 2022 9:00")) {
+  if (new Date() >= new Date("08/28/22 9:00")) {
 
     document.getElementById("time92").innerHTML += "Form (Locked)";
 
