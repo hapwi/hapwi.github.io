1adc4e175919022b70ed154e9da399e1407af982
hapwi <59853329+hapwi@users.noreply.github.com>
2023-02-01 15:35:59 -0700

    update for WM

--- js/maint.js
+++ js/maint.js
@@ -3,7 +3,7 @@
 /* in order to get maintance screen date needs to be on or before current date */
 
 {
-  if (new Date() >= new Date("01/17/23 9:00")) {
+  if (new Date() >= new Date("02/06/23 9:00")) {
     location.href = "http://www.golf.contact/beback.html";
   }
 }
