ded996c65850da47fdde9edeca4ef4f012483aca
hapwi <59853329+hapwi@users.noreply.github.com>
2023-02-08 18:39:01 -0700

    updates

--- js/index.js
+++ js/index.js
@@ -1,27 +1,19 @@
 // lock submissions until tournament start
 {
-
-
-  if (new Date() >= new Date("11/10/22 9:00")) {
-
-    document.getElementById("link92").innerHTML += " <a href='../subs.html' class='menu__link r-link'>Entries</a>";
-
+  if (new Date() >= new Date("02/07/23 9:00")) {
+    document.getElementById("iframe3").innerHTML +=
+      " <a href='../subs.html' class='menu__link r-link'>Entries</a>";
   } else {
-
     document.getElementById("link92").innerHTML += "Entries (Locked)";
   }
 }
 
 // lock form when tournament starts
 {
-  if (new Date() >= new Date("10/25/22 9:00")) {
-
+  if (new Date() >= new Date("02/09/23 9:00")) {
     document.getElementById("time92").innerHTML += "Form (Locked)";
-
   } else {
-
-    document.getElementById("time92").innerHTML += " <a href='../form.html' class='menu__link r-link'>Form</a>";
+    document.getElementById("time92").innerHTML +=
+      " <a href='../form.html' class='menu__link r-link'>Form</a>";
   }
 }
-
-
