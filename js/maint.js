15d73d79535c58611a07b330c917ab0069417cde
hapwi <59853329+hapwi@users.noreply.github.com>
2023-03-27 19:04:30 -0700

    Delete js directory

--- js/maint.js
+++
@@ -1,9 +0,0 @@
-// maintenance
-
-/* in order to get maintance screen date needs to be on or before current date */
-
-{
-  if (new Date() >= new Date("02/13/23 9:00 MST")) {
-    location.href = "http://www.golf.contact/beback.html";
-  }
-}
