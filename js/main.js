bd9eabbaefb58183eddc20c76637e3cdecd2a114
hapwi <59853329+hapwi@users.noreply.github.com>
2023-08-06 16:49:30 -0700

    updated file directory
    updated bottom nav size across all pages

--- js/main.js
+++ js/main.js
@@ -156,10 +156,10 @@ async function showPlayerPicks(event, playerName) {
 }
 
 // //comment out function below before tournament starts so the picks button does not work 
-//  function to display the player's picks inside the picks button
+//! function to display the player's picks inside the picks button
 function displayPlayerPicks(data1, golferScores, playerName) {
     //! Set the date and time when the button should start working
-    const desiredDate = new Date("August 24, 2023 8:00 AM MST"); // Adjust this to your desired date and time
+    const desiredDate = new Date("August 24, 2023 8:00 AM MST"); //? Adjust this to your desired date and time
 
     // Get the current date and time
     const now = new Date();
