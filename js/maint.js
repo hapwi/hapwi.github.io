a6829ed38917031d3efbb282b62d72db84da3098
hapwi <59853329+hapwi@users.noreply.github.com>
2023-08-15 18:39:24 -0700

    maint for gold pool until 10/05

--- js/maint.js
+++ js/maint.js
@@ -3,7 +3,7 @@
 
 
 {
-    if (new Date() <= new Date("08/05/23 9:00 AM MST")) {
+    if (new Date() <= new Date("10/05/23 9:00 AM MST")) {
         location.href = "beback.html"; //! file that is displayed as maintence page
     }
 }
