d2150d552887ff9bcf9a7645664e6f4fabc5659a
hapwi <59853329+hapwi@users.noreply.github.com>
2023-08-22 00:35:45 -0700

    updaated

--- js/maint.js
+++ js/maint.js
@@ -1,9 +1,24 @@
-// ! Maintenance
+// Current date
+const currentDate = new Date();
 
+// Maintenance end date
+const maintenanceDate = new Date("08/05/23 9:00 AM MST");
 
+// Check if the current date is before the maintenance end date
+const isMaintenancePeriod = currentDate <= maintenanceDate;
 
-{
-    if (new Date() <= new Date("08/05/23 9:00 AM MST")) {
-        location.href = "beback.html"; //! file that is displayed as maintence page
-    }
+// Check if the current page is 'beback.html'
+const isOnMaintenancePage = window.location.pathname.endsWith("beback.html");
+
+console.log("Is Maintenance Period:", isMaintenancePeriod);
+console.log("Is On Maintenance Page:", isOnMaintenancePage);
+
+if (isMaintenancePeriod && !isOnMaintenancePage) {
+    console.log("Redirecting to beback.html");
+    location.href = "beback.html"; // Redirect to Maintenance page
+} else if (!isMaintenancePeriod && !window.location.pathname.endsWith("index.html")) {
+    console.log("Redirecting to index.html");
+    location.href = "index.html"; // Redirect to Main page
+} else {
+    console.log("No redirection");
 }
