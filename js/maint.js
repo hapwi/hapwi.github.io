6e07c3f7089a59899f8e1e2e9b754822742f7624
hapwi <59853329+hapwi@users.noreply.github.com>
2023-08-22 01:37:55 -0700

    fixed

--- js/maint.js
+++ js/maint.js
@@ -1,43 +1,29 @@
-// Adding the event listener for the logout button immediately
-let logoutBtn = document.getElementById('logoutBtn');
-if (logoutBtn) {
-    logoutBtn.addEventListener('click', function () {
-        console.log("Logout button clicked!"); // New log statement
-        sessionStorage.removeItem('maintenanceBypass');
-        window.location.href = './beback.html';
-    });
-}
+// Current date
+const currentDate = new Date();
 
+// Maintenance end date
+const maintenanceDate = new Date("09/05/23 9:00 AM MST");
 
+// Check if the current date is before the maintenance end date
+const isMaintenancePeriod = currentDate <= maintenanceDate;
 
-setTimeout(() => {
-    // Current date
-    const currentDate = new Date();
+// Check if the current page is 'beback.html'
+const isOnMaintenancePage = window.location.pathname.endsWith("beback.html");
 
-    // Maintenance end date
-    const maintenanceDate = new Date("09/05/23 9:00 AM MST");
+console.log("Is Maintenance Period:", isMaintenancePeriod);
+console.log("Is On Maintenance Page:", isOnMaintenancePage);
 
-    // Check if the current date is before the maintenance end date
-    const isMaintenancePeriod = currentDate <= maintenanceDate;
+// Check if maintenanceBypass session storage item is set
+const bypassMaintenance = sessionStorage.getItem('maintenanceBypass') === 'true';
 
-    // Check if the current page is 'beback.html'
-    const isOnMaintenancePage = window.location.pathname.endsWith("beback.html");
-
-    console.log("Is Maintenance Period:", isMaintenancePeriod);
-    console.log("Is On Maintenance Page:", isOnMaintenancePage);
-
-    // Check if maintenanceBypass session storage item is set
-    const bypassMaintenance = sessionStorage.getItem('maintenanceBypass') === 'true';
-
-    if (!bypassMaintenance && isMaintenancePeriod) {
-        if (!isOnMaintenancePage) {
-            console.log("Redirecting to beback.html");
-            location.href = "beback.html"; // Redirect to Maintenance page
-        }
-    } else if (!isMaintenancePeriod) {
-        if (!window.location.pathname.endsWith("index.html")) {
-            console.log("Redirecting to index.html");
-            location.href = "index.html"; // Redirect to Main page
-        }
+if (!bypassMaintenance && isMaintenancePeriod) {
+    if (!isOnMaintenancePage) {
+        console.log("Redirecting to beback.html");
+        location.href = "beback.html"; // Redirect to Maintenance page
     }
-}, 100);
+} else if (!isMaintenancePeriod) {
+    if (!window.location.pathname.endsWith("index.html")) {
+        console.log("Redirecting to index.html");
+        location.href = "index.html"; // Redirect to Main page
+    }
+}
