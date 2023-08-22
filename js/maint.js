793821dbd2fe1b2909ac7fadb5e9aaee4e798700
hapwi <59853329+hapwi@users.noreply.github.com>
2023-08-21 21:43:05 -0700

    updates

--- js/maint.js
+++ js/maint.js
@@ -3,7 +3,7 @@
 
 
 {
-    if (new Date() <= new Date("10/05/23 9:00 AM MST")) {
+    if (new Date() <= new Date("09/05/23 9:00 AM MST")) {
         location.href = "beback.html"; //! file that is displayed as maintence page
     }
 }
