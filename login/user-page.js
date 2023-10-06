4477e49be7336161cde15e342aa68332203eea35
hapwi <59853329+hapwi@users.noreply.github.com>
2023-10-05 21:57:51 -0700

    update

--- login/user-page.js
+++ login/user-page.js
@@ -1,11 +1,15 @@
 // Utility to fetch JSON data
 const fetchJSON = async (url) => {
     const response = await fetch(url);
+
     if (!response.ok) {
         throw new Error(`Could not fetch from ${url}`);
     }
     return await response.json();
+
 };
+
+
 
 // Function to load weekly data
 const loadWeekData = async (weekNumber, loggedInUser) => {
@@ -14,24 +18,31 @@ const loadWeekData = async (weekNumber, loggedInUser) => {
         const weekData = allWeeksData[`Week${weekNumber}`];
         const weekDataDiv = document.querySelector("#week-data tbody");
 
-        // Check if weekData exists
+        const weekWinners = winners[weekNumber] || []; // Fetch the winners for the given week
+
         if (weekData) {
             const userData = weekData.find((user) => user.username === loggedInUser);
 
             if (userData) {
-                const pickList = userData.picks.map((pick, idx) => `<tr><td>${idx + 1}</td><td>${pick}</td></tr>`).join("");
+                let pickList = '';
+                for (let idx = 0; idx < userData.picks.length; idx++) {
+                    let pick = userData.picks[idx];
+                    let isWinner = weekWinners.includes(pick) ? 'style="color: green;"' : '';
+                    pickList += `<tr><td ${isWinner}>${idx + 1}</td><td ${isWinner}>${pick}</td></tr>`;
+                }
                 weekDataDiv.innerHTML = `${pickList}<tr><td>Tiebreaker</td><td>${userData.tiebreaker}</td></tr>`;
             } else {
                 weekDataDiv.innerHTML = "<tr><td colspan='2'>No data available for this user this week.</td></tr>";
             }
         } else {
-            // Display this message when no data is found for the selected week
             weekDataDiv.innerHTML = "<tr><td colspan='2'>Picks are not available yet for this week.</td></tr>";
         }
     } catch (error) {
         console.error(`An error occurred: ${error.message}`);
     }
 };
+
+
 
 document.addEventListener("DOMContentLoaded", async () => {
     const dropdown = document.getElementById("week-dropdown");
---
+++ login/winners.js
@@ -0,0 +1,104 @@
+const winners = {
+    "1": [
+        "Detroit Lions (+5.5)",
+        "Green Bay Packers (+1)",
+        "Philadelphia Eagles (-4)",
+        "Atlanta Falcons (-3.5)",
+        "Cleveland Browns (+2.5)",
+        "Jacksonville Jaguars (-5)",
+        "Tampa Bay Buccaneers (+6)",
+        "Tennessee Titans (+3)",
+        "San Francisco 49ers (-2.5)",
+        "Arizona Cardinals (+7)",
+        "Baltimore Ravens (-10)",
+        "Las Vegas Raiders (+4)",
+        "Miami Dolphins (+3)",
+        "Los Angeles Rams (+5.5)",
+        "Dallas Cowboys (-3)",
+        "New York Jets (+2.5)"
+    ], // Week 1
+
+    "2": [
+        "Minnesota Vikings (+7)",
+        "Buffalo Bills (-10)",
+        "Tennessee Titans (+3)",
+        "Tampa Bay Buccaneers (-3)",
+        "Baltimore Ravens (+3.5)",
+        "Indianapolis Colts (-1.5)",
+        "Seattle Seahawks (+6)",
+        "Atlanta Falcons (+1.5)",
+        "Kansas City Chiefs (-3)",
+        "Dallas Cowboys (-3.5)",
+        "Arizona Cardinals (+4.5)",
+        "Los Angeles Rams (+8.5)",
+        "Washington Commanders (+3.5)",
+        "Miami Dolphins (-2.5)",
+        "Carolina Panthers (+3.5)",
+        "Pittsburgh Steelers (+2.5)"
+
+
+    ], // Week 2
+
+    "3": [
+        "San Francisco 49ers (-10)",
+        "New England Patriots (-2.5)",
+        "Detroit Lions (-3.5)",
+        "Buffalo Bills (-6.5)",
+        "Miami Dolphins (-6.5)",
+        "Houston Texans (+9.5)",
+        "Indianapolis Colts (+8)",
+        "Los Angeles Chargers (PK)",
+        "New Orleans Saints (+2)",
+        "Cleveland Browns (-3)",
+        "Arizona Cardinals (+12)",
+        "Kansas City Chiefs (-12.5)",
+        "Seattle Seahawks (-5.5)",
+        "Pittsburgh Steelers (+2.5)",
+        "Philadelphia Eagles (-5)",
+        "Cincinnati Bengals (-2.5)"
+    ],
+    // Week 3
+
+    "4": [
+
+        "Detroit Lions (-1.5)",
+        "Jacksonville Jaguars (-3.0)",
+        "Tennessee Titans (+2.5)",
+        "Tampa Bay Buccaneers (+3.0)",
+        "Washington Commanders (+8.0)",
+        "Buffalo Bills (-3.0)",
+        "Los Angeles Rams (+1.0)",
+        "Baltimore Ravens (+2.5)",
+        "Houston Texans (+3.0)",
+        "Minnesota Vikings (-3.5)",
+        "Los Angeles Chargers (-5.0) ",
+        "San Francisco 49ers (-14.0)",
+        "Dallas Cowboys (-6.5)",
+        "New York Jets (+10.0)",
+        "Seattle Seahawks (+1.0)"
+
+
+    ],
+    // Week 4
+
+    "5": [
+
+        "Chicago Bears (+6)",
+
+    ],
+    // Week 5
+
+    "6": [], // Week 6
+    "7": [], // Week 7
+    "8": [], // Week 8
+    "9": [], // Week 9
+    "10": [], // Week 10
+    "11": [], // Week 11
+    "12": [], // Week 12
+    "13": [], // Week 13
+    "14": [], // Week 14
+    "15": [], // Week 15
+    "16": [], // Week 16
+    "17": [], // Week 17
+    "18": [], // Week 18
+};
