18bd1cbff8fbf9314a1b18ff149ba6bb8a4b1e79
hapwi <59853329+hapwi@users.noreply.github.com>
2023-10-08 16:10:29 -0700

    test

--- login/version.js
+++ login/version.js
@@ -1,4 +1,9 @@
-// version.js
+ // Define a function to replace the version placeholder
+ function updateScriptVersion() {
+     const versionNumber = '1.0.0'; // Replace with the actual version number
+     const scriptElement = document.getElementById('dynamic-script');
+     scriptElement.src = scriptElement.src.replace('{{version}}', versionNumber);
+ }
 
-// Define the version variable
-const version = "1.0.4";
+ // Call the function to update the script source
+ updateScriptVersion();
