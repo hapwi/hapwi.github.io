ee6a07dd2b2658ec71e4a4d6ab61423347246369
hapwi <59853329+hapwi@users.noreply.github.com>
2024-06-30 14:54:06 -0700

    chore: Update GolfPoolLeaderboard component to fix sticky header styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import BottomNav from "./BottomNav";
 import Header from "./Header";
 import AddToHomeScreenPrompt from "./AddToHomeScreenPrompt";
 import ScrollToTop from "./ScrollToTop";
-import "./App.css"; // Import the global CSS
 
 // Create a client
 const queryClient = new QueryClient();
@@ -20,8 +19,8 @@ function App() {
         <div className="App min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-gray-100 flex flex-col">
           <Header />
           <ScrollToTop />
-          <main className="flex-grow pt-28 pb-20 px-4 overflow-hidden">
-            <div className="max-w-2xl mx-auto overflow-hidden">
+          <main className="flex-grow pt-28 pb-20 px-4">
+            <div className="max-w-2xl mx-auto">
               <Routes>
                 <Route path="/" element={<GolfPoolLeaderboard />} />
                 <Route path="/players" element={<Players />} />
