1af1f0681c6f076828a8d05bd1db3c8597f8e3c7
hapwi <59853329+hapwi@users.noreply.github.com>
2024-07-14 17:05:50 -0700

    refactor: Update lazy loading for images in Form.js, Players.js, and GolfPoolLeaderboard.js

--- src/Players.js
+++ src/Players.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { ThemeContext } from "./themeContext";
 
@@ -24,12 +24,41 @@ const Players = () => {
     refetchInterval: 60000,
   });
 
+  useEffect(() => {
+    if (!isLoading && players) {
+    }
+  }, [isLoading, players]);
+
   if (isLoading) {
     return (
       <div
         className={`min-h-screen ${theme.background} flex items-center justify-center`}
       >
-        <div className={theme.text}></div>
+        <button
+          type="button"
+          className="bg-indigo-500 text-white font-bold py-2 px-4 rounded inline-flex items-center"
+          disabled
+        >
+          <svg
+            className="animate-spin h-5 w-5 mr-3 text-white"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Loading...
+        </button>
       </div>
     );
   }
